0f2b72ae50e80f52efcda82dea68fc192658283e
Dimitrios Georgopoulos <dimitrigeo@gmail.com>
2023-04-04 17:52:54 -0300

    Create Exercicio1-2.c

---
+++ Capitulo1/Exercicio1-2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("primeiro programa\a\n");
+    printf("primeiro programa\b\n");
+    printf("primeiro programa\c\n");
+    printf("primeiro programa\d\n");
+    printf("primeiro programa\e\n");
+    printf("\n");
+    printf("primeiro programa\f\n");
+    printf("primeiro programa\g\n");
+    printf("primeiro programa\h\n");
+    printf("primeiro programa\i\n");
+    printf("primeiro programa\j\n");
+    printf("\n");
+    printf("primeiro programa\k\n");
+    printf("primeiro programa\l\n");
+    printf("primeiro programa\m\n");
+    printf("primeiro programa\n\n");
+    printf("primeiro programa\o\n");
+    printf("\n");
+    printf("primeiro programa\p\n");
+    printf("primeiro programa\q\n");
+    printf("primeiro programa\r\n");
+    printf("primeiro programa\s\n");
+    printf("primeiro programa\t\n");
+    printf("\n");
+    //printf("primeiro programa\u\n");
+    printf("primeiro programa\v\n");
+    printf("primeiro programa\w\n");
+    //printf("primeiro programa\x\n");
+    printf("primeiro programa\y\n");
+    printf("\n");
+    printf("primeiro programa\f\z");
+
+
+
+
+}

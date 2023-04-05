647718f6898c6f7273b95378ab0117d737a2b1f5
Dimitrios Georgopoulos <dimitrigeo@gmail.com>
2023-04-05 00:03:14 -0300

    Create Exercicio1-3.c

---
+++ Capitulo1/Exercicio1-3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+float fahr, celsius;
+int inicio, fim, incr;
+
+inicio = 0;
+fim = 550;
+incr = 20;
+
+fahr = inicio;
+
+printf("Fahrenheit \t Celsius\n\n");
+
+while(fahr <= fim)
+{
+    celsius = (5.0/9.0)*(fahr-32);
+    printf("%3.0f \t\t %6.1f\n", fahr, celsius);
+    fahr = fahr + incr;
+}
+
+}
